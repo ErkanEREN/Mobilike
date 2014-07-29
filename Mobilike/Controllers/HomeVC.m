76eb4f7b9d86682564766dcfaa432ea022b817df
Erkan Eren <erkaneren@outlook.com>
2014-07-29 20:20:26 +0300

    Login scene prepared for systems already has twitter installed

--- Mobilike/Controllers/HomeVC.m
+++ Mobilike/Controllers/HomeVC.m
@@ -7,8 +7,12 @@
 //
 
 #import "HomeVC.h"
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
 
 @interface HomeVC ()
+
+@property (strong, nonatomic) NSArray *array;
 
 @end
 
@@ -35,4 +39,64 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)btnTwitterTapped:(UIButton *)sender {
+    
+    [self twittertimeline];
+    
+}
+ 	
+
+
+-(void)twittertimeline{
+    
+    ACAccountStore *account = [[ACAccountStore alloc] init];
+    ACAccountType *accountType = [account accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    
+    [account requestAccessToAccountsWithType:accountType options:nil
+                                  completion:^(BOOL granted, NSError *error)
+                                    {
+                                      if (granted == YES) {
+                                          NSArray *arrayOfAccounts = [account accountsWithAccountType:accountType];
+                                          
+                                          if ([arrayOfAccounts count] > 0)
+                                          {
+                                              ACAccount *twitterAccount = [arrayOfAccounts lastObject];
+                                              // so smthg with accounts
+                                          }
+                                      }
+                                    }];
+    
+    
+    
+}
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
